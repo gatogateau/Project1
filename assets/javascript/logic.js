ce27611678847812e37bd0a1659375011cbe530b
Jerson Hatch <vote4chubs@hotmail.com>
2018-03-03 12:23:44 -0700

    updates

---
+++ assets/javascript/logic.js
@@ -0,0 +1,17 @@
+// script for jarvis environmental group
+
+
+// api key for youtube
+var apiYoutube= ;
+
+
+// api key for googlemaps
+
+var apiMaps= a02d0e73a4f8e0d9c64bddca938d32ea;
+
+// api key for charity foundations
+
+var apiCharity= a02d0e73a4f8e0d9c64bddca938d32ea; 
+
+
+// link search to youtube buttons
