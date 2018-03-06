7408a8e1c1bbfa3e0d49b583581e985eea15b22d
mpras55 <manjunath.bp@gmail.com>
2018-03-05 19:41:23 -0700

    sync

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,21 +6,11 @@
 
 
 // api key for googlemaps
-<<<<<<< HEAD
-
-var apiMaps= a02d0e73a4f8e0d9c64bddca938d32ea;
-
-// api key for charity foundations
-
-var apiCharity= a02d0e73a4f8e0d9c64bddca938d32ea; 
-
-
-// link search to youtube buttons
-=======
 var apiMaps= a02d0e73a4f8e0d9c64bddca938d32ea;
 
 // api key for charity foundations
 var apiCharity= a02d0e73a4f8e0d9c64bddca938d32ea; 
 
 
->>>>>>> master
+//testing commit
+// Manju
